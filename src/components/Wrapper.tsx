f4acd481a34ae45248511cb2015bbc4dfe37cfba
Rajil Bajracharya <imrb1213@gmail.com>
2022-10-06 21:44:50 +0545

    Display dashboard only if user is logged in

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Nav from './Nav'
 import Menu from './Menu'
+import axios from 'axios'
+import { Navigate } from 'react-router-dom'
 
 const Wrapper = ({ children }: React.PropsWithChildren) => {
+
+  const [redirect, setRedirect] = useState(false);
+  
+  useEffect(() => {
+    (
+      async () => {
+        try {
+          await axios.get('user');
+        } catch (e) {
+          // redirect to `Login` page if no user is fetched (unauthenticated)
+          setRedirect(true);
+        }
+      }
+    )();
+  }, [])
+
+  if (redirect) {
+      return <Navigate to="/login"/>
+  }
+
   return (
-      <>
-          <Nav />
-          <div className="container-fluid">
-            <div className="row">
-              <Menu />
-              <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                {children}
-              </main>
-            </div>
+    <>
+        <Nav />
+        <div className="container-fluid">
+          <div className="row">
+            <Menu />
+            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+              {children}
+            </main>
           </div>
-      </>
+        </div>
+    </>
   )
 }
   
