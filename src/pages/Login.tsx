37aae17466d56b6de04646837845c758564d5b6e
Rajil Bajracharya <imrb1213@gmail.com>
2022-10-06 19:36:45 +0545

    Use ES6 function instead of class (for demo)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Login extends Component {
-  render() {
-    return (
-      <div>Login</div>
-    )
-  }
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
 }
 
 export default Login;
