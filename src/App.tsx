5f7026b6dd607c5f788573aa913b68384fdae831
Rajil Bajracharya <imrb1213@gmail.com>
2022-10-06 18:08:48 +0545

    Refactor with Wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import Nav from './components/Nav'
-import Menu from './components/Menu'
 import './App.css';
 import Dashboard from './pages/Dashboard';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Users from './pages/Users';
+import Register from './pages/Register';
 
 function App() {
   return (
       <div className="App">        
         <BrowserRouter>        
-          <Nav />
-          <div className="container-fluid">
-            <div className="row">
-              <Menu />
-              <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                  <Routes>                 
-                    <Route path={'/'} element={<Dashboard />} />
-                    <Route path={'/users'} element={<Users />} />
-                  </Routes>
-              </main>
-            </div>
-          </div>
+          <Routes>                 
+            <Route path={'/'} element={<Dashboard />} />
+            <Route path={'/users'} element={<Users />} />
+            <Route path={'/register'} element={<Register />} />
+          </Routes>
         </BrowserRouter>
       </div>
   );
