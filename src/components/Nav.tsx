4261a0f93418e006fb1a100f7a0b347fea5121e8
Rajil Bajracharya <imrb1213@gmail.com>
2022-10-06 21:44:27 +0545

    Implement Logout functionality

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState }  from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
     const [user, setUser] = useState({
@@ -22,13 +23,19 @@ const Nav = () => {
     // second argument is dependency list, rerender if any [object] changes
     // as this is empty, this will run once
 
+    const logout = async () => {
+        await axios.post("logout", {});
+    }
+
     return (
         <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
             <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Company name</a>
 
             <ul className="my-2 my-md-0 mr-md-3">
-                <a href="#" className="p2 text-white text-decoration-none">{user.first_name}</a> 
-                <a href="#" className="p-2 text-white">Sign out</a>
+                <Link to="/profile" className="p2 text-white text-decoration-none">{user.first_name}</Link> 
+                <Link to="/login" className="p-2 text-white" 
+                    onClick={logout}
+                >Sign out</Link>
             </ul>
         </nav>
     )
