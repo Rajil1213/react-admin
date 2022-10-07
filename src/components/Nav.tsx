05a0106f6d47b9c4d07928b744384d3a58e78d1e
Rajil Bajracharya <imrb1213@gmail.com>
2022-10-07 18:19:02 +0545

    Use the user model

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState }  from "react";
 import { Link } from "react-router-dom";
+import { User } from "../models/user";
 
 const Nav = () => {
-    const [user, setUser] = useState({
-        first_name: ''
-    });
+    const [user, setUser] = useState(new User());
 
     // analogous to componentDidMount
     useEffect(() => {
@@ -15,7 +14,12 @@ const Nav = () => {
                 const {data} = await axios.get("user")
 
                 if (data.id) {
-                    setUser(data);
+                    setUser(new User(
+                        data.id,
+                        data.first_name,
+                        data.last_name,
+                        data.email
+                    ));
                 }
             }
         )();
@@ -32,7 +36,7 @@ const Nav = () => {
             <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Company name</a>
 
             <ul className="my-2 my-md-0 mr-md-3">
-                <Link to="/profile" className="p2 text-white text-decoration-none">{user.first_name}</Link> 
+                <Link to="/profile" className="p2 text-white text-decoration-none">{user.name}</Link> 
                 <Link to="/login" className="p-2 text-white" 
                     onClick={logout}
                 >Sign out</Link>
