dea5dfd9a883bab99737384c1b19aa10431fc26f
Rajil Bajracharya <imrb1213@gmail.com>
2022-10-15 18:45:10 +0545

    Add routes to add/edit products

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import Roles from './pages/roles/Roles';
 import RoleCreate from './pages/roles/RoleCreate';
 import RoleEdit from './pages/roles/RoleEdit';
 import Products from './pages/products/Products';
+import ProductCreate from './pages/products/ProductCreate';
+import ProductEdit from './pages/products/ProductEdit';
 
 function App() {
   return (
@@ -26,6 +28,8 @@ function App() {
             <Route path={'/roles/create'} element={<RoleCreate/>}/>
             <Route path={'/roles/:id/edit'} element={<RoleEdit/>}/>
             <Route path={'/products'} element={<Products />}/>
+            <Route path={'/products/create'} element={<ProductCreate/>}/>
+            <Route path={'/products/:id/edit'} element={<ProductEdit/>}/>
           </Routes>
         </BrowserRouter>
       </div>
