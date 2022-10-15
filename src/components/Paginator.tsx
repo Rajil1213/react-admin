9cfa5ce625be4c137360576e73923d42599d1e15
Rajil Bajracharya <imrb1213@gmail.com>
2022-10-15 19:36:22 +0545

    Create separate component for pagination

---
+++ src/components/Paginator.tsx
@@ -0,0 +1,41 @@
+import React  from 'react'
+
+const Paginator = (props: {
+    page: number, 
+    lastPage: number, 
+    pageChanged: (page: number) => void
+}) => {
+
+    /**
+     * Call Paginator with:
+     * @param page The current page
+     * @param lastPage The `lastPage` for pagination
+     * @param pageChange An emitter function that increments the value of page, props.pageChanged(pageVal) === setPage(pageVal) (in parent component)
+     */
+
+
+    const next = () => {
+        let pageVal = props.page < props.lastPage? props.page + 1: props.lastPage;
+        props.pageChanged(pageVal);
+    }
+
+    const previous = () => {
+        let pageVal = props.page > 1? props.page - 1: 1;
+        props.pageChanged(pageVal);
+    }
+
+    return (
+        <nav>
+            <ul className="pagination">
+                <li className="page-item">
+                    <a href="#" className="page-link" onClick={previous}>Previous</a>
+                </li>
+                <li className="page-item">
+                    <a href="#" className="page-link" onClick={next}>Next</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Paginator;
