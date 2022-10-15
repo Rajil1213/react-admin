3630d02e970663f72dcae48cc4b4488a89f7620c
Rajil Bajracharya <imrb1213@gmail.com>
2022-10-16 02:12:18 +0545

    Use Wrapper to make "user" API call and update store

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,43 +1,71 @@
-import React, { useEffect, useState } from 'react'
+import React, { Dispatch, useEffect, useState } from 'react'
 import Nav from './Nav'
 import Menu from './Menu'
 import axios from 'axios'
 import { Navigate } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { User } from '../models/user'
+import { setUser } from '../redux/reducers/setUserReducer'
 
-const Wrapper = ({ children }: React.PropsWithChildren) => {
+const Wrapper = (props: any) => {
 
-  const [redirect, setRedirect] = useState(false);
+    const [redirect, setRedirect] = useState(false);
+    
+    const { children, setUser} = props;
   
-  useEffect(() => {
-    (
-      async () => {
-        try {
-          await axios.get('user');
-        } catch (e) {
-          // redirect to `Login` page if no user is fetched (unauthenticated)
-          setRedirect(true);
-        }
-      }
-    )();
-  }, [])
+    useEffect(() => {
+        (
+            async () => {
+                try {
+                    const { data } = await axios.get('user');
 
-  if (redirect) {
-      return <Navigate to="/login"/>
-  }
+                    setUser(new User(
+                        data.id,
+                        data.first_name,
+                        data.last_name,
+                        data.email,
+                        data.role
+                    ));
 
-  return (
-    <>
-        <Nav />
-        <div className="container-fluid">
-          <div className="row">
-            <Menu />
-            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-              {children}
-            </main>
-          </div>
-        </div>
-    </>
+                } catch (e) {
+                    // redirect to `Login` page if no user is fetched (unauthenticated)
+                    console.log(e);
+                    console.log("Redirecting to login page")
+                    setRedirect(true);
+                }
+            }
+        )();
+    }, [setUser])
+
+    if (redirect) {
+        return <Navigate to="/login"/>
+    }
+
+    return (
+        <>
+            <Nav />
+            <div className="container-fluid">
+                <div className="row">
+                    <Menu />
+                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                        {children}
+                    </main>
+                </div>
+            </div>
+        </>
   )
 }
-  
-export default Wrapper;
+
+const mapStatetoProps = (state: {user: User}) => {
+    return {
+        user: state.user
+    }
+}
+
+const mapDispatchtoProps = (dispatch: Dispatch<any>) => {
+    return {
+        setUser: (user: User) => dispatch(setUser(user))
+    }
+}
+
+export default connect(mapStatetoProps, mapDispatchtoProps)(Wrapper);
