66c79e9b75a838377717a58d0a5e6789db4efd2e
Rajil Bajracharya <imrb1213@gmail.com>
2022-10-16 02:10:35 +0545

    Create actionCreater, deprecated

---
+++ src/redux/actions/setUserAction.ts
@@ -0,0 +1,13 @@
+/*
+* Legacy Redux code no longer necessary, handled by RTK in `setUserReducer`
+
+import { User } from "../../models/user";
+export const setUser = (user: User) => {
+    return {
+        type: "SET_USER",
+        user
+    }
+}
+*/
+// this required for compilation
+export {}
